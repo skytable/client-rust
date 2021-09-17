15d088c7d760d6aee44865e98597b63b3853b123
Sayan Nandan <nandansayan@outlook.com>
2021-09-17 06:55:15 -0700

    Add bin derive

--- sky-derive/src/lib.rs
+++ sky-derive/src/lib.rs
@@ -30,11 +30,11 @@ pub fn derive_json(item: TokenStream) -> TokenStream {
             if let Fields::Named(named_fields) = st.fields {
                 let a = named_fields.named;
                 if a.is_empty() {
-                    panic!("`sky_derive::bin` will not work on fieldless structs")
+                    panic!("`sky_derive::Skyjson` will not work on fieldless structs")
                 }
             }
         }
-        _ => panic!("`sky_derive::bin` is only implemented for structs"),
+        _ => panic!("`sky_derive::Skyjson` is only implemented for structs"),
     }
 
     let tokens = quote! {
@@ -47,6 +47,42 @@ pub fn derive_json(item: TokenStream) -> TokenStream {
             fn from_element(e: skytable::Element) -> skytable::SkyRawResult<Self> {
                 let s: String = e.try_element_into()?;
                 match skytable::derive::from_json_bytes(&s) {
+                    Ok(s) => Ok(s),
+                    Err(e) => Err(skytable::error::Error::ParseError(e.to_string())),
+                }
+            }
+        }
+    };
+    TokenStream::from(tokens)
+}
+
+#[proc_macro_derive(Skybin)]
+pub fn derive_bin(item: TokenStream) -> TokenStream {
+    let x: DeriveInput = syn::parse_macro_input!(item);
+    let struct_name = x.ident;
+
+    match x.data {
+        Data::Struct(st) => {
+            if let Fields::Named(named_fields) = st.fields {
+                let a = named_fields.named;
+                if a.is_empty() {
+                    panic!("`sky_derive::Skybin` will not work on fieldless structs")
+                }
+            }
+        }
+        _ => panic!("`sky_derive::Skybin` is only implemented for structs"),
+    }
+
+    let tokens = quote! {
+        impl skytable::types::IntoSkyhashBytes for &#struct_name {
+            fn as_bytes(&self) -> Vec<u8> {
+                skytable::derive::into_bin_bytes(&self)
+            }
+        }
+        impl skytable::types::FromSkyhashBytes for #struct_name {
+            fn from_element(e: skytable::Element) -> skytable::SkyRawResult<Self> {
+                let s: Vec<u8> = e.try_element_into()?;
+                match skytable::derive::from_bin_bytes(&s) {
                     Ok(s) => Ok(s),
                     Err(e) => Err(skytable::error::Error::ParseError(e.to_string())),
                 }
